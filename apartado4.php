69696b40c7a73470404b1def5c70440bcd64b5b0
Miguel <miguelalbencamunoz.guadalupe@alumnado.fundacionloyola.net>
2025-11-05 20:06:44 +0100

    Delete apartado4.php

--- apartado4.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include "config.php";
-    $resultado = $conexion->query("SELECT * FROM ciudades");
-    while ($fila = $resultado->fetch_array()) {
-        echo 'ID: ' . $fila['idCiudades'] . ' - Nombre: ' . $fila['nombre'] . '</br>';
-    }
-?>
